59f7a51538c296104ddb67998c499861eb4454a3
_CHAI__ <apichai.freedom@gmail.com>
2021-09-03 18:07:35 +0700

    b

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var assert = require('assert');
 
-describe('String#split______', function(){
+describe('String#split_____', function(){
   it('should return an array', function(){
     assert(Array.isArray('a,b,c'.split(',')));
   });
